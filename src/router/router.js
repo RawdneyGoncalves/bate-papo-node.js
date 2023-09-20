0d93c18efa8df893c18e1697f8d1ea156a57ef0f
Lucas Mangolin <lucas971453307@gmail.com>
2023-09-20 17:33:01 -0300

    O usuário agora pode enviar mensagens

--- src/router/router.js
+++ src/router/router.js
@@ -3,3 +3,28 @@ const routes = express.Router();
 
 
 const userController = require('../controllers/userController');
+const Message  = require('./models/messageModel'); // Importe o modelo de mensagem
+
+
+app.post('/sendMessage', async (req, res) => {
+    try {
+        const { mensagem, username } = req.body
+
+        // nova mensagem
+        const novaMensagem = new Message({
+            mensagem,
+            username,
+            dataEnvio: new Date(),
+        })
+
+        await novaMensagem.save()
+
+        res.status(201).json({ message: 'A mensagem foi enviada com sucesso.' });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Erro ao enviar mensagem!' });
+      }
+
+    }
+)
