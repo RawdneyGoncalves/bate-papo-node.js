648d4a1650eff344743a9d0fa8e704101c2fa719
Rawdney Mendes <mendesrawdney@gmail.com>
2023-10-09 23:52:18 -0300

    fix: refactoring index, implemetation socket.io/ refatoring methods/ remove comments

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
 const app = express();
-const http = require('http').createServer(app); 
-const io = require('socket.io')(http); 
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 const PORT = process.env.PORT || 3030;
 
-
-
-// conectar ao mongoose
 require('./src/database/index');
 
-//pp.options("*", cors());
-
-// importar router
 const routes = require("./src/router/router.js");
 
-//Middleware que recebe um valor em json 
 app.use(express.json());
 
-app.use(express.static(__dirname + './src/public'));
+app.use(express.static(__dirname + '/src/public'));
 
+io.on('connection', (socket) => {
+    console.log('Um cliente se conectou');
+
+    socket.on('disconnect', () => {
+        console.log('Um cliente se desconectou');
+    });
+
+    socket.on('chat message', (message) => {
+        messageController.postMessage({ body: { message } }, { sendStatus: () => { } });
+    });
+});
 
 app.use(routes);
 
-http.listen(PORT, () => { 
+http.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
+
+module.exports = {
+    io,
+  };
