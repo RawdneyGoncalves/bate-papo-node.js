72da6158297bdbc2d7bcec4042b2c64ff72fc97e
Lucas Mangolin <lucas971453307@gmail.com>
2023-10-20 15:58:36 -0300

    patch: getMessage removed because is not used

--- src/router/router.js
+++ src/router/router.js
@@ -7,19 +7,17 @@ const messageController = require('../controllers/messageController');
 
 routes.route('/users/').get(userController.getAllUser);
 
-routes.route('/users/:id').get (userController.getUser)
+routes.route('/users/:id').get(userController.getUser)
 
-routes.route('/register').post (userController.createUser);
+routes.route('/register').post(userController.createUser);
 
-routes.route('/users/:id').put (userController.updateUser)
+routes.route('/users/:id').put(userController.updateUser)
 
 routes.route('/deleteUser/:id').delete(userController.deleteUser);
 
 
 
 routes.route('/messages').post(messageController.postMessage);
-
-routes.route('/messages').get(messageController.getMessage);
 
 module.exports = routes;
 
